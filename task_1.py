f6545436aaa67b5835dc273a4bc5afcd9bf46dab
Vlad <vlad07407@gmail.com>
2024-09-06 17:14:08 +0500

    Changed nod function

--- task_1.py
+++ task_1.py
@@ -1,9 +1,7 @@
 def nod(x, y):
     for i in range(y, 0, -1):
         if x % i == 0 and y % i == 0:
-            print(f'НОД: {i}')
-            quit()
-    print(f'Общего делителя не существует')
+            return (f'НОД: {i}')
 
 num_1 = int(input('Введите первое число: '))
 num_2 = int(input('Введите второе число: '))
@@ -14,9 +12,9 @@ elif num_1 < num_2:
     if num_2 % num_1 == 0:
         print(f'НОД: {num_1}')
     else:
-        nod(num_2, num_1)
+        print(nod(num_2, num_1))
 else:
     if num_1 % num_2 == 0:
         print(f'НОД: {num_2}')
     else:
-        nod(num_1, num_2)
+        print(nod(num_1, num_2))
