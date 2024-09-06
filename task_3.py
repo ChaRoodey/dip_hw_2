e5e9cc0ae3176feb66b8c16f8fe8b837a3681aa2
Vlad <vlad07407@gmail.com>
2024-09-06 17:26:40 +0500

    task_3 completed

--- task_3.py
+++ task_3.py
@@ -1,3 +1,21 @@
-CONV = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 1000:'M'}
+CONV = {
+        1000:'M', 900:'СM', 500:'D', 400:'CD',
+        100:'C', 90:'XC', 50:'L', 40:'XL',
+        10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I',
+        }
 
-num = int(input('Введите число: '))
+num = int(input('Введите число от -3999 до 3999: '))
+rez = ''
+
+print(f'Римское представление числа "{num}" : ', end='')
+
+if num <= 0:
+    num = abs(num)
+    rez = '-'
+
+for key, value in CONV.items():
+    while num >= key:
+        num -= key
+        rez += value
+
+print(rez)
